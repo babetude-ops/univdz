25bc76ed09374a9c83e7fa17de15447986beefb8
Babetude <babetude@gmail.com>
2026-03-03 13:58:52 +0100

    fix production database URL

--- config/settings.py
+++ config/settings.py
@@ -5,16 +5,16 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class DevelopmentConfig:
     DEBUG = True
-    SECRET_KEY = "univdz-secret-key-2024"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "univdz-secret-key-2024")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "..", "univdz.db")
 
 
 class ProductionConfig:
     DEBUG = False
-    SECRET_KEY = "univdz-secret-key-2024"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "univdz-secret-key-2024")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "..", "univdz.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
 
 
 config = {
